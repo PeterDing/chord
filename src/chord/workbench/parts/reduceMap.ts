39c3752f432d19f30d3eb45285338deaec8fce51
PeterDing <dfhayst@gmail.com>
2018-09-21 12:11:52 +0800

    Add menu reducer to main reduce map

--- src/chord/workbench/parts/reduceMap.ts
+++ src/chord/workbench/parts/reduceMap.ts
@@ -3,6 +3,7 @@
 import { IReducerMap } from 'chord/workbench/api/common/reducer/reducerMap';
 import { playerReducer } from 'chord/workbench/parts/player/browser/reducer/main';
 import { mainViewReducer } from 'chord/workbench/parts/mainView/browser/reducer/main'
+import { menuReducer } from 'chord/workbench/parts/menu/browser/reducer/main';
 
 
 export function map(act: string): IReducerMap {
@@ -15,6 +16,11 @@ export function map(act: string): IReducerMap {
         return {
             reducer: mainViewReducer,
             node: 'mainView',
+        };
+    } else if (act.startsWith('c:menu:')) {
+        return {
+            reducer: menuReducer,
+            node: 'menu',
         };
     } else {
         return null;
