3bdadf82e9574b236513669c0e4202127ce3df8f
PeterDing <dfhayst@gmail.com>
2018-09-21 12:00:43 +0800

    Add menu state at `IStateGlobal`

--- src/chord/workbench/api/common/state/stateGlobal.ts
+++ src/chord/workbench/api/common/state/stateGlobal.ts
@@ -2,11 +2,13 @@
 
 import { IPlayerState, initiatePlayerState } from 'chord/workbench/api/common/state/player';
 import { IMainViewState, initiateMainViewState } from 'chord/workbench/api/common/state/mainView/mainView';
+import { IMenuState, initiateMenuState } from 'chord/workbench/api/common/state/menu/menu';
 
 
 export interface IStateGlobal {
     player: IPlayerState;
     mainView: IMainViewState;
+    menu: IMenuState;
 }
 
 /**
@@ -16,5 +18,6 @@ export function initiateStateGlobal(): IStateGlobal {
     return {
         player: initiatePlayerState(),
         mainView: initiateMainViewState(),
+        menu: initiateMenuState(),
     };
 }
