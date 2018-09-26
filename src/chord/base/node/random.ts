a8b7e451efa0b8ff839a761d85ff26ef0173c638
PeterDing <dfhayst@gmail.com>
2018-09-26 09:37:19 +0800

    Add `getRandomSample`

--- src/chord/base/node/random.ts
+++ src/chord/base/node/random.ts
@@ -5,16 +5,26 @@ export function getRandom() {
   return Math.random();
 }
 
-export function getRandomInt(min, max) {
+export function getRandomInt(min: number, max: number): number {
     min = Math.ceil(min);
     max = Math.floor(max);
     //The maximum and the minimum are inclusive
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export function getRandomFloat(min, max) {
+export function getRandomFloat(min: number, max: number): number {
     min = Math.ceil(min);
     max = Math.floor(max);
     //The maximum and the minimum are inclusive
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export function getRandomSample(array: Array<any>, size: number): Array<any> {
+    let range = array.length;
+    let result = [];
+    while (result.length < size) {
+        let index = Math.floor(getRandom() * range);
+        result.push(array[index]);
+    }
+    return result;
 }
