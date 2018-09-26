7cbd186c77c4411b973e48cd1057d92f9c7549a8
PeterDing <dfhayst@gmail.com>
2018-09-26 09:35:35 +0800

    Origins are wrote to be full name, NOT abbreviated

--- src/chord/music/common/origin.ts
+++ src/chord/music/common/origin.ts
@@ -1,9 +1,35 @@
 'use strict';
 
+import { ok } from 'chord/base/common/assert';
 
+
+// TODO: change to full name
 export enum ORIGIN {
-    xiami = 'xm',
-    netease = 'ne',
+    xiami = 'xiami',
+    netease = 'netease',
+}
+
+export interface IOriginType {
+    origin: string;
+
+    // type: 'song', 'artist', 'album', 'collection'
+    type: string;
+
+    // original id
+    id: string;
+}
+
+export function getOrigin(id: string): IOriginType {
+    ok(Array.from(id).filter(c => c == '|').length == 2, `[ERROR] [getOrigin]: id is not a chord id, ${id}`);
+
+    let chunk = id.split('|');
+    let origin = ORIGIN[chunk[0]];
+    let type = chunk[1];
+    let originId = chunk[2];
+
+    ok(origin, `[ERROR] [getOrigin]: unknown origin: ${origin}, id: ${id}`);
+
+    return { origin, type, id: originId };
 }
 
 export enum OriginSongUrl {
@@ -28,19 +54,19 @@ export enum OriginCollectionUrl {
 
 
 export function getSongId(origin: string, id: string): string {
-    return `${ORIGIN[origin]}|s|${id}`;
+    return `${ORIGIN[origin]}|song|${id}`;
 }
 
 export function getAlbumId(origin: string, id: string): string {
-    return `${ORIGIN[origin]}|a|${id}`;
+    return `${ORIGIN[origin]}|album|${id}`;
 }
 
 export function getArtistId(origin: string, id: string): string {
-    return `${ORIGIN[origin]}|t|${id}`;
+    return `${ORIGIN[origin]}|artist|${id}`;
 }
 
 export function getCollectionId(origin: string, id: string): string {
-    return `${ORIGIN[origin]}|c|${id}`;
+    return `${ORIGIN[origin]}|collection|${id}`;
 }
 
 export function getSongUrl(origin: string, id: string): string {
