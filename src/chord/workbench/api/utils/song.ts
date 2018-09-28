9534ca56fc2274e80835c0239313046eb4ad7bc8
PeterDing <dfhayst@gmail.com>
2018-09-28 18:11:21 +0800

    Add `addSongAudios`, `hasSongAudioPath`
    * addSongAudios add new audio url to song, except audio of the song
      has `path`
    * `hasSongAudioPath` returns true, only if audios of the song has `path`

--- src/chord/workbench/api/utils/song.ts
+++ src/chord/workbench/api/utils/song.ts
@@ -1,7 +1,18 @@
 'use strict';
 
+import { ORIGIN } from 'chord/music/common/origin';
+
 import { ISong } from 'chord/music/api/song';
 
+import { musicApi } from 'chord/music/core/api';
+
+
+export function hasSongAudioPath(song: ISong): boolean {
+    for (let audio of song.audios) {
+        return !!(audio.path);
+    }
+    return false;
+}
 
 export function hasSongAudio(song: ISong): boolean {
     for (let audio of song.audios) {
@@ -12,4 +23,17 @@ export function hasSongAudio(song: ISong): boolean {
 
 export function filterSongWithAudios(songs: Array<ISong>): Array<ISong> {
     return songs.filter(song => hasSongAudio(song));
+}
+
+export async function addSongAudios(song: ISong) {
+    // netease's audio needs to be got by realtime
+    if (song.origin == ORIGIN.netease) {
+        if (!hasSongAudioPath(song)) {
+            song.audios = await musicApi.audios(song.songId);
+        }
+    } else {
+        if (!hasSongAudio(song)) {
+            song.audios = await musicApi.audios(song.songId);
+        }
+    }
 }
