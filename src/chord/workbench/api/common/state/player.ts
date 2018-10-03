b5884ecd20f6b2ef7d2e08d68aa572dac6b64ff2
PeterDing <dfhayst@gmail.com>
2018-10-03 15:17:15 +0800

    Add `kbps` for player state

--- src/chord/workbench/api/common/state/player.ts
+++ src/chord/workbench/api/common/state/player.ts
@@ -7,7 +7,8 @@ export type IPlayerList = Array<ISong>;
 
 export interface IPlayerState {
     // currently playing song's index of play list
-    index: number | null;
+    index: number;
+    kbps: number;
     playList: IPlayerList;
     volume: number;
     playing: boolean;
@@ -17,6 +18,7 @@ export interface IPlayerState {
 export function initiatePlayerState() {
     return {
         index: null,
+        kbps: null,
         playList: [],
         volume: 0.5,
         playing: false,
