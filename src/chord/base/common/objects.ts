02709c62fe7e00ed0f38f40a90acee0e4445bfc8
PeterDing <dfhayst@gmail.com>
2018-10-03 15:16:12 +0800

    Add `removeEmtryAttributes`

--- src/chord/base/common/objects.ts
+++ src/chord/base/common/objects.ts
@@ -5,3 +5,12 @@ export function assign(destination: any, ...sources: any[]): any {
     sources.forEach(source => Object.keys(source).forEach(key => destination[key] = source[key]));
     return destination;
 }
+
+export function removeEmtryAttributes(obj: any) {
+    Object.keys(obj).forEach(key => {
+        let val = obj[key];
+        if (val === null || val === undefined) {
+            delete obj[key];
+        }
+    });
+}
