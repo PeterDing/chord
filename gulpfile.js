50289c8a869fdde80ad8a4e493c46beb56d4ba19
PeterDing <dfhayst@gmail.com>
2018-10-25 11:13:53 +0800

    Using gulp 4.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,41 @@
 var gulp = require('gulp');
-var tsb = require('gulp-tsb');
+var { series } = require('gulp');
+var ts = require('gulp-typescript');
 var filter = require('gulp-filter');
 var clean = require('gulp-clean');
 var path = require('path');
 var fs = require('fs');
 var rimraf = require('rimraf');
 
-var opts = require('./src/tsconfig.json').compilerOptions;
+var ts_opts = require('./src/tsconfig.json').compilerOptions;
 var dest = './out';
-
-var compilation = tsb.create(opts);
 
 
 function cleanOut() {
-    return gulp.src(dest, {read: false})
+    return gulp.src(dest, { read: false })
         .pipe(clean());
 }
 
-function copy() {
+function copyFiles() {
     const other = filter(file => !/\.tsx?$/.test(file.path));
     return gulp.src('./src/**')
         .pipe(other)
         .pipe(gulp.dest(dest));
 }
 
-function compile() {
+function compileTS() {
     const ts_or_tsx = filter(file => /\.ts|\.tsx/.test(file.path));
-    const ndts = filter(file => !/\.d\.ts/.test(file.path));
-    const output = gulp.src('./src/**')
+    const no_dts = filter(file => !/\.d\.ts/.test(file.path));
+    return gulp.src('./src/**')
         .pipe(ts_or_tsx)
-        .pipe(ndts)
-        .pipe(compilation())
+        .pipe(no_dts)
+        .pipe(ts(ts_opts))
+        .on('error', () => {})
         .pipe(gulp.dest(dest));
-    return output;
 }
 
 
-gulp.task('clean', cleanOut);
-gulp.task('copy', ['clean'], copy);
-gulp.task('compile', ['copy'], compile);
-gulp.task('default', ['compile']);
+exports.clean = cleanOut;
+exports.copy = series(cleanOut, copyFiles);
+exports.compile = series(copyFiles, compileTS);
+exports.default = compileTS;
