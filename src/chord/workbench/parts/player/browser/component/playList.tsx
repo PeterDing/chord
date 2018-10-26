d43958acbbdcb199f264022395e1319a37828342
PeterDing <dfhayst@gmail.com>
2018-10-26 14:27:39 +0800

    Change Playlist IU

--- src/chord/workbench/parts/player/browser/component/playList.tsx
+++ src/chord/workbench/parts/player/browser/component/playList.tsx
@@ -3,34 +3,131 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 
-import { IPlayListProps } from 'chord/workbench/parts/player/browser/props/playList';
+import { getDateYear } from 'chord/base/common/time';
+import { ESize } from 'chord/music/common/size';
+
+import { IPlayListButtomProps, IPlayListContentProps, IPlayListSongDetailProps } from 'chord/workbench/parts/player/browser/props/playList';
 import { IStateGlobal } from 'chord/workbench/api/common/state/stateGlobal';
 import { handlePlay } from 'chord/workbench/parts/player/browser/action/playList';
 import SongItemView from 'chord/workbench/parts/common/component/songItem';
 
+import { AlbumIcon } from 'chord/workbench/parts/common/component/common';
+import { OriginIcon } from 'chord/workbench/parts/common/component/originIcons';
 
-class PlayList extends React.Component<IPlayListProps, object> {
+import { handleShowArtistViewById } from 'chord/workbench/parts/mainView/browser/action/showArtist';
+import { handleShowAlbumViewById } from 'chord/workbench/parts/mainView/browser/action/showAlbum';
+import { handleAddLibrarySong } from 'chord/workbench/parts/mainView/browser/action/addLibraryItem';
 
-    playList: React.RefObject<HTMLDivElement>;
-    show: boolean;
+import { musicApi } from 'chord/music/core/api';
 
-    constructor(props: IPlayListProps) {
+
+class PlayListSongDetail extends React.Component<IPlayListSongDetailProps, any> {
+
+    constructor(props: IPlayListSongDetailProps) {
         super(props);
-        this.showPlayList = this.showPlayList.bind(this);
-
-        this.playList = React.createRef();
-        this.show = false;
     }
-
-    showPlayList() {
-        this.show = !this.show;
-        this.playList.current.style.display = this.show ? 'block' : 'none';
-    }
-
 
     render() {
+        let song = this.props.song;
+        if (!song) {
+            return null;
+        }
+
+        let cover = song.albumCoverPath || musicApi.resizeImageUrl(song.origin, song.albumCoverUrl, ESize.Large);
+        let originIcon = OriginIcon(song.origin, 'left-icon xiami-icon');
+
+        // TODO: Get `like` real-time from library
+        let likeIconClass = song.like ? 'spoticon-heart-active-16' : 'spoticon-heart-16';
+
+        return (
+            <div className='playlist-content-song-detail'>
+                <div className="cover-art shadow cover-art--with-auto-height" aria-hidden="true"
+                    style={{ width: '200px', height: '200px', padding: 0 }}>
+                    <div>
+                        {AlbumIcon}
+                        <div className="cover-art-image cover-art-image-loaded"
+                            style={{ backgroundImage: `url("${cover}")` }}>
+                        </div>
+                    </div>
+                </div>
+
+                <div style={{ display: 'inline-grid', width: 'inherit' }}>
+                    {/* Origin Icon */}
+                    <div className="mo-meta ellipsis-one-line">
+                        <div className="react-contextmenu-wrapper">
+                            <span>{originIcon}</span>
+                        </div>
+                    </div>
+
+                    {/* Song Name */}
+                    <div className="mo-info">
+                        <div className="react-contextmenu-wrapper">
+                            <span className="mo-info-name link-subtle">{song.songName}</span>
+                        </div>
+                    </div>
+
+                    {/* Album Name */}
+                    <div className="mo-info">
+                        <div className="react-contextmenu-wrapper"
+                            onClick={() => this.props.handleShowAlbumViewById(song.albumId)}>
+                            <span className="mo-info-name link-subtle">{song.albumName}</span>
+                        </div>
+                    </div>
+
+
+                    {/* Artist Name */}
+                    <div className="mo-meta ellipsis-one-line">
+                        <div className="react-contextmenu-wrapper"
+                            onClick={() => this.props.handleShowArtistViewById(song.artistId)}>
+                            <span className="link-subtle">{song.artistName}</span>
+                        </div>
+                    </div>
+
+                    {/* Publish Date */}
+                    <div className="mo-meta ellipsis-one-line">
+                        <div className="react-contextmenu-wrapper">
+                            <span>{getDateYear(song.releaseDate)}</span>
+                        </div>
+                    </div>
+
+                    {/* Like */}
+                    <div className="mo-info">
+                        <div className="react-contextmenu-wrapper">
+                            <button className={`control-button ${likeIconClass}`}
+                                title="Save to your Favorite Songs"
+                                onClick={() => this.props.handleAddLibrarySong(song)}>
+                            </button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+
+function mapDispatchToPropsForSongDetail(dispatch) {
+    return {
+        handleShowArtistViewById: (artistId: string) => handleShowArtistViewById(artistId).then(act => dispatch(act)),
+        handleShowAlbumViewById: (albumId: string) => handleShowAlbumViewById(albumId).then(act => dispatch(act)),
+        handleAddLibrarySong: (song) => dispatch(handleAddLibrarySong(song)),
+    };
+}
+
+const _PlayListSongDetail = connect(null, mapDispatchToPropsForSongDetail)(PlayListSongDetail);
+
+
+class PlayListContent extends React.Component<IPlayListContentProps, any> {
+
+    constructor(props: IPlayListContentProps) {
+        super(props);
+    }
+
+    render() {
+        let playList = this.props.playList;
         let index = this.props.index;
-        let songItems = this.props.playList.map((song, i) => (
+        let songItems = playList.map((song, i) => (
             <SongItemView
                 key={i.toString()}
                 handlePlay={() => this.props.handlePlay(i)}
@@ -40,14 +137,46 @@ class PlayList extends React.Component<IPlayListProps, object> {
                 thumb={true} />
         ));
 
+        let song = playList[index];
+
+        return (
+            <div>
+                <div className='playlist-content-songs'>
+                    {songItems}
+                </div>
+
+                <_PlayListSongDetail song={song} />
+            </div>
+        );
+    }
+}
+
+
+export class PlayListButtom extends React.Component<IPlayListButtomProps, any> {
+
+    playListContent: React.RefObject<HTMLDivElement>;
+    show: boolean;
+
+    constructor(props: IPlayListButtomProps) {
+        super(props);
+        this.showPlayList = this.showPlayList.bind(this);
+
+        this.playListContent = this.props.playListContent;
+        this.show = false;
+    }
+
+    showPlayList() {
+        this.show = !this.show;
+        this.playListContent.current.style.display = this.show ? 'flex' : 'none';
+    }
+
+
+    render() {
         return (
             <div className='player-list-container'>
                 <button className='spoticon-queue-16 control-button'
                     onClick={this.showPlayList} aria-label='Queue'>
                 </button>
-                <div className='player-list-songs' ref={this.playList}>
-                    {songItems}
-                </div>
             </div>
         );
     }
@@ -56,8 +185,7 @@ class PlayList extends React.Component<IPlayListProps, object> {
 
 function mapStateToProps(state: IStateGlobal) {
     return {
-        index: state.player.index,
-        playList: state.player.playList,
+        ...state.player,
     };
 }
 
@@ -67,5 +195,5 @@ function mapDispatchToProps(dispatch) {
     };
 }
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(PlayList);
+const _PlayListContent = connect(mapStateToProps, mapDispatchToProps)(PlayListContent);
+export { _PlayListContent as PlayListContent, _PlayListSongDetail as PlayListSongDetail };
--- src/chord/workbench/parts/player/browser/props/playList.ts
+++ src/chord/workbench/parts/player/browser/props/playList.ts
@@ -1,11 +1,29 @@
 'use strict';
 
+import * as React from 'react';
+
 import { ISong } from 'chord/music/api/song';
+
+import { IShowArtistAct, IShowAlbumAct } from 'chord/workbench/api/common/action/mainView';
+import { IAddLibrarySongAct } from 'chord/workbench/api/common/action/mainView';
 import { IPlayAct } from 'chord/workbench/api/common/action/player';
 
+import { IPlayerState } from 'chord/workbench/api/common/state/player';
 
-export interface IPlayListProps {
-    index: number;
-    playList: Array<ISong>;
+
+export interface IPlayListButtomProps {
+    playListContent: React.RefObject<HTMLDivElement>;
+}
+
+
+export interface IPlayListContentProps extends IPlayerState {
     handlePlay: (index: number) => Promise<IPlayAct>;
+}
+
+
+export interface IPlayListSongDetailProps {
+    song: ISong;
+    handleShowArtistViewById: (artistId: string) => Promise<IShowArtistAct>;
+    handleShowAlbumViewById: (albumId: string) => Promise<IShowAlbumAct>;
+    handleAddLibrarySong: (song: ISong) => IAddLibrarySongAct;
 }
