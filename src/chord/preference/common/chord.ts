b6fe77a99d0f6607f7416fe2b631f72e6544014d
PeterDing <dfhayst@gmail.com>
2018-11-07 13:17:44 +0800

    Change `CHORD_DIR` for Windows

--- src/chord/preference/common/chord.ts
+++ src/chord/preference/common/chord.ts
@@ -2,6 +2,15 @@
 
 import * as paths from 'path';
 import * as os from 'os';
+import * as fs from 'fs';
+
+import { isWindows } from 'chord/base/common/platform';
+import { mkdirp } from 'chord/base/node/pfs';
 
 
-export const CHORD_DIR = paths.join(os.homedir(), '.chord');
+export const CHORD_DIR = isWindows ?
+    paths.join(os.homedir(), 'Appdata', 'Roaming', 'chord') : paths.join(os.homedir(), '.chord');
+
+if (!fs.existsSync(CHORD_DIR)) {
+    mkdirp(CHORD_DIR);
+}
