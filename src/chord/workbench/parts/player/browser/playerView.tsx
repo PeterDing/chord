fbc41ee000e8616a764dbf5e1552e39b13b62a10
PeterDing <dfhayst@gmail.com>
2018-11-18 15:20:14 +0800

    Support toggling player's controller
    * click the playing song's image to toggle player's controller

--- src/chord/workbench/parts/player/browser/playerView.tsx
+++ src/chord/workbench/parts/player/browser/playerView.tsx
@@ -7,7 +7,6 @@ import * as React from 'react';
 import ProcessBar from 'chord/workbench/parts/player/browser/component/processBar';
 import { Cover, SongInfo } from 'chord/workbench/parts/player/browser/component/songInfo';
 import LikeButton from 'chord/workbench/parts/player/browser/component/like';
-// import Like from 'chord/workbench/parts/player/browser/component/like';
 import Controller from 'chord/workbench/parts/player/browser/component/controller';
 import VolumeBar from 'chord/workbench/parts/player/browser/component/volume';
 import { PlayListButtom, PlayListContent } from 'chord/workbench/parts/player/browser/component/playList';
@@ -18,9 +17,33 @@ export class PlayerView extends React.Component<any, any> {
 
     playListContentRef: React.RefObject<HTMLDivElement>;
 
+    playerBarContainer: React.RefObject<HTMLDivElement>;
+    playerBarCenter: React.RefObject<HTMLDivElement>;
+    playerBarRight: React.RefObject<HTMLDivElement>;
+    toggleState: boolean;
+
+
     constructor(props: any) {
         super(props);
+
+        this.toggleController = this.toggleController.bind(this);
+
         this.playListContentRef = React.createRef();
+
+        this.playerBarContainer = React.createRef();
+        this.playerBarCenter = React.createRef();
+        this.playerBarRight = React.createRef();
+        this.toggleState = false;
+    }
+
+    toggleController() {
+        let containerAnimation = `player-bar-controller-toggle-${this.toggleState ? 'off' : 'on'} 1s ease 0s 1 normal forwards`;
+
+        this.playerBarContainer.current.style.animation = containerAnimation;
+        this.playerBarCenter.current.style.display = this.toggleState ? 'block' : 'none';
+        this.playerBarRight.current.style.display = this.toggleState ? 'flex' : 'none';
+
+        this.toggleState = !this.toggleState;
     }
 
     render() {
@@ -30,26 +53,26 @@ export class PlayerView extends React.Component<any, any> {
                     <PlayListContent />
                 </div>
 
-                <div className='playerBar-container'>
+                <div ref={this.playerBarContainer} className='playerBar-container'>
                     <footer className='player-bar-container'>
                         <div className='player-bar'>
 
                             <div className='player-bar__left'>
                                 <div className='now-player'>
-                                    <Cover />
+                                    <Cover onclick={() => this.toggleController()} />
                                     <SongInfo />
                                     <LikeButton />
                                 </div>
                             </div>
 
-                            <div className='player-bar__center'>
+                            <div ref={this.playerBarCenter} className='player-bar__center'>
                                 <div className='player-controls'>
                                     <Controller />
                                     <ProcessBar />
                                 </div>
                             </div>
 
-                            <div className='player-bar__right'>
+                            <div ref={this.playerBarRight} className='player-bar__right'>
                                 <div className='player-bar__right__inner'>
                                     <div className='extra-controls'>
                                         <AudioInfo />
