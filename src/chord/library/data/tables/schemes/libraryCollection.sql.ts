b063fbfd5de9ffedaf0fca8677143631a4ed1fe2
PeterDing <dfhayst@gmail.com>
2018-11-24 15:13:41 +0800

    Fix library database `library_collection` table column type
    * Fix releaseDate, duration, songCount type
      changing `text` to `integer`

--- src/chord/library/data/tables/schemes/libraryCollection.sql.ts
+++ src/chord/library/data/tables/schemes/libraryCollection.sql.ts
@@ -19,13 +19,36 @@ const TABLE_COLUMNS = [
     ['userId', 'text'],
     ['userMid', 'text'],
     ['userName', 'text'],
-    ['releaseDate', 'text'],
+
+    /**
+     * ['releaseDate', 'integer']
+     * Typo
+     *
+     * change to integer
+     */
+    ['releaseDate', 'integer'],
+
     ['description', 'text'],
     ['tags', 'text'],
-    ['duration', 'text'],
+
+    /**
+     * ['duration', 'text']
+     * Typo
+     *
+     * change to integer
+     */
+    ['duration', 'integer'],
     // json array ["songId"]
     ['songs', 'text'],
-    ['songCount', 'text'],
+
+    /**
+     * ['songCount', 'text'],
+     * Typo
+     *
+     * change to integer
+     */
+    ['songCount', 'integer'],
+
     ['playCount', 'integer'],
     ['likeCount', 'integer'],
     ['like', 'integer'],
@@ -41,4 +64,30 @@ export function makeTable(db: Database): void {
 
 export function fixTable(db: Database): void {
     addMissColumns(TABLE_NAME, TABLE_COLUMNS, db);
+
+    // Fix releaseDate, duration, songCount type error
+    // {{{
+    let tableScheme = db.prepare(`PRAGMA table_info(${TABLE_NAME})`).all();
+    let songCountCol = tableScheme.find(columnInfo => columnInfo.name == 'songCount' && columnInfo.type == 'text');
+    if (songCountCol) {
+        let tmpTableName = TABLE_NAME + '_' + Date.now();
+
+        // Create a temporary table
+        let tableSql = makeTableSql(tmpTableName, TABLE_COLUMNS, ADDONS);
+        db.exec(tableSql);
+
+        let sql;
+        // Update table
+        sql = `INSERT INTO ${tmpTableName} SELECT * FROM ${TABLE_NAME}`;
+        db.exec(sql);
+
+        // Remove old table
+        sql = `DROP TABLE ${TABLE_NAME}`;
+        db.exec(sql);
+
+        // Use new table
+        sql = `ALTER TABLE ${tmpTableName} RENAME TO ${TABLE_NAME}`;
+        db.exec(sql);
+    }
+    // }}}
 }
