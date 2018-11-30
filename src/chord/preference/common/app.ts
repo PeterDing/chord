efb9e0ac37415a5dbfedbe58c343a083f52ade59
PeterDing <dfhayst@gmail.com>
2018-11-30 11:41:34 +0800

    Add `APP_SEARCH_HISTORY_PATH` to store searching history

--- src/chord/preference/common/app.ts
+++ src/chord/preference/common/app.ts
@@ -6,3 +6,4 @@ import { CHORD_DIR } from 'chord/preference/common/chord';
 
 
 export const APP_CONFIGURATION_PATH = paths.join(CHORD_DIR, 'chord.config');
+export const APP_SEARCH_HISTORY_PATH = paths.join(CHORD_DIR, 'chord-search-history.json');
