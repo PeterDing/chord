be11191f367065355754609ee6ffb36b4103a602
PeterDing <dfhayst@gmail.com>
2019-01-10 20:34:44 +0800

    Add `htmlGet` function for simple http GET request

--- src/chord/base/node/_request.ts
+++ src/chord/base/node/_request.ts
@@ -15,3 +15,23 @@ export async function request(options: IRequestOptions): Promise<IResponse> {
     let resp = await _requestPromise(options);
     return resp;
 }
+
+
+const DEFAULT_HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+};
+
+const DEFAULT_OPTIONS: (url: string) => IRequestOptions =
+    (url: string) => {
+        return {
+            method: 'GET',
+            url: url,
+            headers: DEFAULT_HEADERS,
+            gzip: true,
+            resolveWithFullResponse: false,
+        };
+    };
+
+export async function htmlGet(url: string): Promise<IResponse> {
+    return request(DEFAULT_OPTIONS(url));
+}
