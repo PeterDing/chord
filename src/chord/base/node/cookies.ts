604c0348c612d523903dced554af40246e75d12b
PeterDing <dfhayst@gmail.com>
2019-01-15 15:59:33 +0800

    Fix tough-cookie errors

--- src/chord/base/node/cookies.ts
+++ src/chord/base/node/cookies.ts
@@ -1,31 +1,27 @@
 'use strict';
 
 import { Cookie } from 'tough-cookie';
-import * as rp from 'request-promise';
-import { CookieJar } from 'request';
+import * as r from 'request';
 
 
 export type Cookie = Cookie;
-export type CookieJar = CookieJar;
+export type CookieJar = r.CookieJar;
 
 
 export function makeCookieJar(): CookieJar {
-    return rp.jar();
+    return r.jar();
 }
 
 export function makeCookies(rawCookies: Array<string>, jar?: CookieJar): Array<Cookie> {
-    let cookies = [];
-    rawCookies.forEach(r => {
-        let cookie = Cookie.parse(r);
-        cookies.push(cookie);
-    });
+    let cookies = rawCookies.map(rawCookie => makeCookieFrom(rawCookie));
     return cookies;
 }
 
 export function makeCookie(key: string, value: string, domain?: string): Cookie {
-    return Cookie.fromJSON({ key, value, domain });
+    let s = `${key}=${value}; expires=; max-age=; path=/; domain=${domain ? domain : ''}`;
+    return makeCookieFrom(s);
 }
 
 export function makeCookieFrom(input: string): Cookie {
-    return Cookie.parse(input);
+    return r.cookie(input);
 }
