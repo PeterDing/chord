8ab13c7de0630036331e2ba69ebda75080d4f28f
PeterDing <dfhayst@gmail.com>
2019-04-06 11:24:04 +0800

    Change `libraryView` background

--- src/chord/workbench/electron-browser/media/mainBackground.ts
+++ src/chord/workbench/electron-browser/media/mainBackground.ts
@@ -8,7 +8,7 @@ const MAIN_BACKGROUND_MAP = {
     // 'albumView': 'linear-gradient(rgb(30, 50, 100), rgb(4, 6, 12) 85%)',
     'artistView': 'linear-gradient(rgb(18, 18, 18), rgb(8, 8, 8) 85%)',
     // 'collectionView': 'linear-gradient(rgb(102, 56, 51), rgb(10, 6, 5) 85%)',
-    'libraryView': 'linear-gradient(rgb(132, 25, 54), rgb(6, 8, 10) 85%)',
+    'libraryView': 'linear-gradient(rgb(41, 50, 60), rgb(5, 7, 11) 85%)',
     'preferenceView': '-webkit-gradient(linear, left top, left bottom, from(#A0C3D2), to(#509BF5))',
     'userProfileView': 'linear-gradient(rgb(133, 156, 179), rgb(1, 1, 11) 85%)',
     'homeView': 'linear-gradient(rgb(55, 93, 113), rgb(5, 9, 11) 85%)',
