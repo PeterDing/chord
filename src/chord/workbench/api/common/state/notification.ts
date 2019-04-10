4d5f2e61365e71060dbbe125f292bd8908b5534d
PeterDing <dfhayst@gmail.com>
2019-04-10 14:31:29 +0800

    Add `INotificationState`

---
+++ src/chord/workbench/api/common/state/notification.ts
@@ -0,0 +1,39 @@
+'use strict';
+
+
+/**
+ * Notice types
+ */
+export enum NOTICES {
+    PLAY_SONG,
+    PLAY_ARTIST,
+    PLAY_ALBUM,
+    PLAY_COLLECTION,
+    PLAY_USERPROFILE,
+    PLAY_LIST,
+
+    // No song can be played
+    NO_SONG,
+    // Filter songs which have no audio.
+    FILTER_SONGS,
+    // Song which has no audio.
+    NO_AUDIO,
+}
+
+
+// T is the item the notice occured.
+export interface INotice<T> {
+    type: NOTICES;
+    item: T;
+    message: string,
+}
+
+
+export interface INotificationState {
+    entries: Array<INotice<any>>;
+}
+
+
+export function initiateNotificationState(): INotificationState {
+    return { entries: [] };
+}
