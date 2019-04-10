4966b9e8caa3b5878c574bc7c0a15ad99a9cdd94
PeterDing <dfhayst@gmail.com>
2019-04-10 14:42:45 +0800

    Add actions and reducers for notification

--- src/chord/workbench/parts/reduceMap.ts
+++ src/chord/workbench/parts/reduceMap.ts
@@ -4,6 +4,7 @@ import { IReducerMap } from 'chord/workbench/api/common/reducer/reducerMap';
 import { playerReducer } from 'chord/workbench/parts/player/browser/reducer/main';
 import { mainViewReducer } from 'chord/workbench/parts/mainView/browser/reducer/main'
 import { menuReducer } from 'chord/workbench/parts/menu/browser/reducer/main';
+import { notificationReducer } from 'chord/workbench/parts/notification/reducer/main';
 
 
 export function map(act: string): IReducerMap {
@@ -16,6 +17,11 @@ export function map(act: string): IReducerMap {
         return {
             reducer: mainViewReducer,
             node: 'mainView',
+        };
+    } else if (act.startsWith('c:notification:')) {
+        return {
+            reducer: notificationReducer,
+            node: 'notification',
         };
     } else if (act.startsWith('c:menu:')) {
         return {
