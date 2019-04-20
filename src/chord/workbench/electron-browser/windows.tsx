38823a3fd38dc29fa51679ae9df97c540d8627a6
PeterDing <dfhayst@gmail.com>
2019-04-20 12:13:43 +0800

    Add window's controllor for UI
    * Add window's controllor view and css
    
    * The window's controllor only use to Windows UI

---
+++ src/chord/workbench/electron-browser/windows.tsx
@@ -0,0 +1,66 @@
+'use strict';
+
+import * as React from 'react';
+import { remote, shell } from 'electron';
+
+
+export default class WindowsControlBar extends React.Component<any, any> {
+
+    constructor(props: any) {
+        super(props);
+    }
+
+    min() {
+        let win = remote.getCurrentWindow();
+        win.minimize();
+    }
+
+    max() {
+        let win = remote.getCurrentWindow();
+        win.maximize();
+    }
+
+    close() {
+        let win = remote.getCurrentWindow();
+        win.close();
+    }
+
+    toggleDevTools() {
+        let win = remote.getCurrentWindow();
+        win.webContents.toggleDevTools();
+    }
+
+    report() {
+        shell.openExternal('https://github.com/PeterDing/chord/issues');
+    }
+
+    render() {
+        return (
+            <div className='window-controls'>
+                <div className='window-controls-container'>
+
+                    <div className='window-icon-bg window-close-bg' onClick={this.close}>
+                        <div className='window-icon window-close cursor-pointer'></div>
+                    </div>
+
+                    <div className='window-icon-bg' onClick={this.min}>
+                        <div className='window-icon window-minimize cursor-pointer'></div>
+                    </div>
+
+                    <div className='window-icon-bg' onClick={this.max}>
+                        <div className='window-icon window-max-restore window-maximize cursor-pointer'></div>
+                    </div>
+
+                    <div className='window-icon-bg' title='Toggle DevTools' onClick={this.toggleDevTools}>
+                        <div className='window-icon dev-tools cursor-pointer'></div>
+                    </div>
+
+                    <div className='window-icon-bg' title='report' onClick={this.report}>
+                        <div className='window-icon report cursor-pointer'></div>
+                    </div>
+
+                </div>
+            </div>
+        );
+    }
+}
