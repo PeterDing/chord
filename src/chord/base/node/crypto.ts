062b67fb4b60ab6cafa818c223051d2900b4a988
PeterDing <dfhayst@gmail.com>
2019-05-09 21:47:02 +0800

    Support aes

--- src/chord/base/node/crypto.ts
+++ src/chord/base/node/crypto.ts
@@ -18,3 +18,41 @@ export function decodeBase64(data: string): string {
     if (!data) return data;
     return Buffer.from(data, 'base64').toString();
 }
+
+
+export function encryptAES(
+    buf: string,
+    key: Buffer,
+    iv: Buffer,
+    inputEncoding?: string,
+    outputEncoding?: string,
+    mode: string = 'cbc',
+): string {
+    let algorithm = 'aes-' + key.length * 8 + '-' + mode;
+
+    const cipher = crypto.createCipheriv(algorithm, key, iv);
+
+    let encrypted = cipher.update(buf, inputEncoding as any, outputEncoding as any);
+    encrypted += cipher.final(outputEncoding);
+
+    return encrypted;
+}
+
+
+export function decryptAES(
+    buf: string,
+    key: Buffer,
+    iv: Buffer,
+    inputEncoding?: string,
+    outputEncoding?: string,
+    mode: string = 'cbc',
+): string {
+    let algorithm = 'aes-' + key.length * 8 + '-' + mode;
+
+    const decipher = crypto.createDecipheriv(algorithm, key, iv);
+
+    let decrypted = decipher.update(buf, inputEncoding as any, outputEncoding as any);
+    decrypted += decipher.final(outputEncoding);
+    return decrypted;
+}
+
