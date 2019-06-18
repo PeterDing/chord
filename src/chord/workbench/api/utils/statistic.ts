9a112506a2c1d9f753e69cc6dcbd37e21ddaddcb
PeterDing <dfhayst@gmail.com>
2019-06-18 19:38:14 +0800

    Handle episode, podcast, radio play count info

--- src/chord/workbench/api/utils/statistic.ts
+++ src/chord/workbench/api/utils/statistic.ts
@@ -7,7 +7,11 @@ import { IArtist } from 'chord/music/api/artist';
 import { ICollection } from 'chord/music/api/collection';
 import { IUserProfile } from 'chord/music/api/user';
 
-export function getLikeAndPlayCount(item: IAlbum | IArtist | ICollection): string {
+import { IPodcast } from 'chord/sound/api/podcast';
+import { IRadio } from 'chord/sound/api/radio';
+
+
+export function getLikeAndPlayCount(item: IAlbum | IArtist | ICollection | IPodcast): string {
     let likeAndPlayCount = [
         item.likeCount ? literal_number(item.likeCount) + ' like' : null,
         item.playCount ? literal_number(item.playCount) + ' play' : null,
@@ -15,14 +19,29 @@ export function getLikeAndPlayCount(item: IAlbum | IArtist | ICollection): strin
     return likeAndPlayCount;
 }
 
+
 /**
  * Get user's following, follower and favorite song count
  */
-export function getUserProfileCount(item: IUserProfile): string {
+export function getUserProfileCount(item: IUserProfile | IRadio): string {
+    let trackCount;
+    let trackType;
+    switch (item.type) {
+        case 'userProfile':
+            trackCount = (item as IRadio).episodeCount;
+            trackType = ' songs';
+            break;
+        case 'radio':
+            trackCount = (item as IRadio).episodeCount;
+            trackType = ' episodes';
+            break;
+        default:
+            break;
+    }
     let userProfileCount = [
         item.followerCount ? literal_number(item.followerCount) + ' followers' : null,
         item.followingCount ? literal_number(item.followingCount) + ' following' : null,
-        item.songCount? literal_number(item.songCount) + ' songs' : null,
+        trackCount ? literal_number(trackCount) + trackType : null,
     ].filter(i => !!i).join(' • ');
     return userProfileCount;
 }
