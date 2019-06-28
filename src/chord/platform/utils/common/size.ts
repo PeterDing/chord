4a1644bf5b1bb0cc18139eb17c98d935cdf07d6c
PeterDing <dfhayst@gmail.com>
2019-06-28 10:37:14 +0800

    Add `literal_to_number`

--- src/chord/platform/utils/common/size.ts
+++ src/chord/platform/utils/common/size.ts
@@ -17,3 +17,28 @@ export function literal_number(num: number): string {
     num /= OneK;
     return num.toFixed(1) + 'G';
 }
+
+
+export function literal_to_number(literal: string): number {
+    if (!literal) return 0;
+
+    if (typeof literal == 'number') return literal;
+
+    let num = Number.parseFloat(literal);
+    literal = literal.toLowerCase();
+    let c = literal.slice(-1);
+    if ('kmg'.includes(c)) {
+        switch (c) {
+            case 'k':
+                num *= OneK;
+                break;
+            case 'm':
+                num *= OneK * OneK;
+                break;
+            case 'm':
+                num *= OneK * OneK * OneK;
+                break;
+        }
+    }
+    return num;
+}
