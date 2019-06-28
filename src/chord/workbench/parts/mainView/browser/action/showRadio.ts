73735e65cc498c53ab857dc83dcbbafaf1e53440
PeterDing <dfhayst@gmail.com>
2019-06-28 10:45:39 +0800

    Notice "No Radio" if we can not get radios

--- src/chord/workbench/parts/mainView/browser/action/showRadio.ts
+++ src/chord/workbench/parts/mainView/browser/action/showRadio.ts
@@ -6,8 +6,14 @@ import { IRadio } from 'chord/sound/api/radio';
 
 import { soundApi } from 'chord/sound/core/api';
 
+import { noticeInfo } from 'chord/workbench/parts/notification/action/notice';
+
 
 export async function handleShowRadioView(radio: IRadio): Promise<IShowRadioAct> {
+    if (!radio) {
+        noticeInfo('Radio', 'No Radio');
+        return null;
+    }
     return {
         type: 'c:mainView:showRadioView',
         act: 'c:mainView:showRadioView',
