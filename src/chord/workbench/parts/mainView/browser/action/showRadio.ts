b8af77e03a670e7abb4044f6127877e15fd2b99d
PeterDing <dfhayst@gmail.com>
2019-07-01 11:07:15 +0800

    Handle unavailable radios

--- src/chord/workbench/parts/mainView/browser/action/showRadio.ts
+++ src/chord/workbench/parts/mainView/browser/action/showRadio.ts
@@ -1,5 +1,7 @@
 'use strict';
 
+import { getOrigin } from 'chord/music/common/origin';
+
 import { IShowRadioAct } from 'chord/workbench/api/common/action/mainView';
 
 import { IRadio } from 'chord/sound/api/radio';
@@ -10,10 +12,6 @@ import { noticeInfo } from 'chord/workbench/parts/notification/action/notice';
 
 
 export async function handleShowRadioView(radio: IRadio): Promise<IShowRadioAct> {
-    if (!radio) {
-        noticeInfo('Radio', 'No Radio');
-        return null;
-    }
     return {
         type: 'c:mainView:showRadioView',
         act: 'c:mainView:showRadioView',
@@ -24,5 +22,10 @@ export async function handleShowRadioView(radio: IRadio): Promise<IShowRadioAct>
 
 export async function handleShowRadioViewById(radioId: string): Promise<IShowRadioAct> {
     let radio = await soundApi.radio(radioId);
+    if (!radio) {
+        let { origin } = getOrigin(radioId);
+        noticeInfo('Radio', origin + ` hasn't radios`);
+        return null;
+    }
     return handleShowRadioView(radio);
 }
