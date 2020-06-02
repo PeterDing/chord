dc7fa26c5879be62cc3954215c8a226fe46cdbbc
PeterDing <dfhayst@gmail.com>
2020-06-02 14:00:08 +0800

    No save searching history at quit

--- src/chord/workbench/electron-browser/action/quit.ts
+++ src/chord/workbench/electron-browser/action/quit.ts
@@ -5,13 +5,10 @@ const logger = debug('chord:electron-browser');
 
 import { ipcRenderer } from 'electron';
 
-import { writeLocalSearchHistoryState } from 'chord/preference/utils/app';
-
 
 // Handle quit signal
 export function handleQuit(): void {
     ipcRenderer.on('chord-quit', () => {
         logger('[event]: chord-quit')
-        writeLocalSearchHistoryState();
     });
 }
