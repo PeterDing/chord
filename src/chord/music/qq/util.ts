a15d3ddae4834144e95db37a9f5069d5a69016d8
PeterDing <dfhayst@gmail.com>
2020-07-03 13:56:06 +0800

    Check empty argument

--- src/chord/music/qq/util.ts
+++ src/chord/music/qq/util.ts
@@ -1,12 +1,28 @@
 'use strict';
 
+import { IMap } from 'chord/base/common/map';
 
-export function getACSRFToken(cookies: { [key: string]: string }): string {
-    function e(e) {
+
+export function getACSRFToken(cookies: IMap<string>): string {
+    function e(x) {
+        if (!x) {
+            return '';
+        }
         let n, o, t;
-        for (n = 5381, o = 0, t = e.length; t > o; ++o)
-            n += (n << 5) + e.charCodeAt(o);
+        for (n = 5381, o = 0, t = x.length; t > o; ++o)
+            n += (n << 5) + x.charCodeAt(o);
         return (2147483647 & n).toString();
     }
-    return e(cookies["p_skey"] || cookies["skey"] || cookies["p_lskey"] || cookies["lskey"]);
+    return e(
+        cookies['qqmusic_key'] ||
+        cookies['p_skey'] ||
+        cookies['skey'] ||
+        cookies['p_lskey'] ||
+        cookies['lskey'] ||
+        cookies['skey'] ||
+        cookies['p_skey'] ||
+        cookies['p_lskey'] ||
+        cookies['lskey'] ||
+        cookies['qqmusic_key']
+    );
 }
