88755fead6a9d363d4977756831e485159803910
PeterDing <dfhayst@gmail.com>
2020-07-19 11:18:52 +0800

    Update

--- src/chord/workbench/parts/player/browser/action/control.ts
+++ src/chord/workbench/parts/player/browser/action/control.ts
@@ -12,34 +12,25 @@ import { addPlayItemAudios } from 'chord/workbench/api/utils/playItem';
 import { handlePlay } from 'chord/workbench/parts/player/browser/action/playList';
 
 
-let SHUFFLE_SONG_ID_LIST: Array<number>;
+let SHUFFLE_SONG_ID_LIST: Array<number> = [];
 let PLAYLIST_ID: number;
 
 
-function getShuffleIndex(max: number, originalIndex: number): number {
-    let { id: playListId, playList } = (<any>window).store.getState().player;
-    if (playListId != PLAYLIST_ID || SHUFFLE_SONG_ID_LIST.length == 0) {
+function shuffleIndex(): number {
+    let { playList } = (<any>window).store.getState().player;
+    if (SHUFFLE_SONG_ID_LIST.length == 0) {
         SHUFFLE_SONG_ID_LIST = getRandomSample([...Array(playList.length).keys()], playList.length);
-        PLAYLIST_ID = playListId;
     }
-
-    return SHUFFLE_SONG_ID_LIST.pop();
-}
-
-
-export async function handleShuffle(): Promise<IPlayAct> {
-    let state: IStateGlobal = (<any>window).store.getState();
-
-    let index = getShuffleIndex(Math.max(state.player.playList.length - 1, 0), state.player.index);
-
-    return handlePlay(index);
+    let index = SHUFFLE_SONG_ID_LIST.pop();
+    return index;
 }
 
 
 export async function handleRewind(): Promise<IPlayAct> {
     let player: IPlayerState = (window as any).store.getState().player;
     if (player.shuffle) {
-        return handleShuffle();
+        let index = shuffleIndex();
+        return handlePlay(index);
     }
 
     let state: IStateGlobal = (<any>window).store.getState();
@@ -67,7 +58,8 @@ export async function handlePlayPause(): Promise<IPlayPauseAct> {
 export async function handleForward(): Promise<IPlayAct> {
     let player: IPlayerState = (window as any).store.getState().player;
     if (player.shuffle) {
-        return handleShuffle();
+        let index = shuffleIndex();
+        return handlePlay(index);
     }
 
     let state: IStateGlobal = (<any>window).store.getState();
