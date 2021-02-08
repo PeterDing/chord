675926f75847dc8d29b54afbc66f285ecd6a9d60
PeterDing <dfhayst@gmail.com>
2021-02-08 10:52:03 +0800

    Add func `isOriginAlive` to check alive origin

--- src/chord/unity/api/items.ts
+++ src/chord/unity/api/items.ts
@@ -1,7 +1,21 @@
 'use strict';
 
 import { ISong } from 'chord/music/api/song';
+import { ORIGIN } from 'chord/music/common/origin';
 import { IEpisode } from 'chord/sound/api/episode';
 
 
 export type TPlayItem = ISong | IEpisode;
+
+export enum UnavailableOrigins {
+    // xiami has shutdown its server.
+    xiami = ORIGIN.xiami,
+}
+
+export function isOriginAlive(origin: ORIGIN): boolean {
+    if (UnavailableOrigins[origin]) {
+        return false;
+    } else {
+        return true;
+    }
+}
