28862b6e86fd84e2de00733b5cf574d116a08b36
PeterDing <dfhayst@gmail.com>
2022-05-15 18:09:18 +0800

    Change npm to yarn

--- build/common/computeNodeModulesCacheKey.js
+++ build/common/computeNodeModulesCacheKey.js
@@ -11,7 +11,7 @@ const ROOT = path.join(__dirname, '../../');
 const shasum = crypto.createHash('sha1');
 shasum.update(fs.readFileSync(path.join(ROOT, 'build/.cachesalt')));
 
-// Add `package.json`
+// Add `package.json` and `yarn.lock` files
 const packageJsonPath = path.join(ROOT, 'package.json');
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath).toString());
 const relevantPackageJsonSections = {
@@ -22,6 +22,8 @@ const relevantPackageJsonSections = {
     resolutions: packageJson.resolutions
 };
 shasum.update(JSON.stringify(relevantPackageJsonSections));
+const yarnLockPath = path.join(ROOT, 'yarn.lock');
+shasum.update(fs.readFileSync(yarnLockPath));
 
 // Add any other command line arguments
 for (let i = 2; i < process.argv.length; i++) {
