043ffa58c5a1e58293f80d419d733107ad895a9e
PeterDing <dfhayst@gmail.com>
2022-06-07 11:23:02 +0800

    Use `axios` instead of `request`

--- src/chord/base/node/_request.ts
+++ src/chord/base/node/_request.ts
@@ -1,18 +1,24 @@
 'use strict';
 
-import * as _requestPromise from 'request-promise';
-import * as _request from 'request';
+import axios from 'axios';
+import { AxiosRequestConfig, AxiosResponse } from 'axios';
+import { wrapper } from 'axios-cookiejar-support';
+
+import { CookieJar } from 'chord/base/node/cookies';
+
+// For node use HTTP adapter
+// https://github.com/axios/axios/issues/552#issuecomment-262884004
+if (global.axiosAdaptersHttp) {
+    axios.defaults.adapter = global.axiosAdaptersHttp;
+}
+
+// Let axios to support cookiejar
+// https://github.com/3846masa/axios-cookiejar-support/blob/fa23f1ac07/examples/wrap_static_axios.mjs
+wrapper(axios);
 
 
 interface IMaxRetries {
     maxRetries?: number;
-}
-
-export type IRequestOptions = IMaxRetries & _requestPromise.Options;
-export type IResponse = _request.Response;
-
-export async function request(options: IRequestOptions): Promise<IResponse> {
-    return await _requestPromise(options);
 }
 
 
@@ -20,17 +26,22 @@ const DEFAULT_HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
 };
 
-const DEFAULT_OPTIONS: (url: string, headers?: any) => IRequestOptions =
-    (url: string, headers?: any) => {
-        return {
-            method: 'GET',
-            url: url,
-            headers: { ...DEFAULT_HEADERS, ...(headers || {}) },
-            gzip: true,
-            resolveWithFullResponse: false,
-        };
-    };
+// Extended Request Config
+// https://github.com/3846masa/axios-cookiejar-support#extended-request-config
+declare module 'axios' {
+    interface AxiosRequestConfig {
+        jar?: CookieJar;
+    }
+}
+
+export type IRequestOptions = AxiosRequestConfig;
+export type IResponse = AxiosResponse;
+
+export async function request(url: string, options?: IRequestOptions): Promise<IResponse> {
+    let resp = await axios(url, options);
+    return resp;
+}
 
 export async function htmlGet(url: string, headers?: any): Promise<IResponse> {
-    return request(DEFAULT_OPTIONS(url, headers));
+    return await request(url, { method: 'GET', headers: { ...DEFAULT_HEADERS, ...(headers || {}) } });
 }
